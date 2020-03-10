28ba842d9bf71404798e1d18eeba1f0fad167a4e
emeka ewelike <emekaewelike@gmail.com>
2020-03-10 01:33:12 -0700

    added search elements to dom

--- script/script.js
+++ script/script.js
@@ -1,9 +1,19 @@
 $(function(){
-	$('#smallscreens').click(function(){
+	$('#Market-hub-smallscreen').click(function(){
 		$('#ul-smallscreen1').css({'display':'block'})
+
 	})
 	$('#smallscreens').dblclick(function(){
 		$('#ul-smallscreen1').css({'display':'none'})
+		
+	})
+	$('#Skill-Trade-Hub-smallscreen').click(function(){
+		$('#ul-smallscreen2').css({'display':'block'})
+
+	})
+	$('#smallscreens').dblclick(function(){
+		$('#ul-smallscreen2').css({'display':'none'})
+		
 	})
 	$('.carousel').carousel({
   		interval: 4000
